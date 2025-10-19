0c80510c8293c2ad883ba2f5ef17da880ee07093
thepufferfish <ian.huff2@gmail.com>
2025-10-19 15:47:39 -0700

    feat: move dagster to parent directory

--- src/litmatch/defs/assets.py
+++ src/litmatch/defs/assets.py
@@ -1,17 +1,20 @@
 import os
+import json
 
-from typing import List
+from typing import List, Dict
 
 import dagster as dg
+
+from backend.db.models import Book
 
 RAW_DATA_DIR = '/home/framework/.local/share/containers/storage/volumes/litmatch_shared_scraper_output/_data/raw'
 
 @dg.asset
-def extract(context: dg.AssetExecutionContext) -> List[str]:
+def extract(context: dg.AssetExecutionContext) -> List[Dict]:
 
     fn = os.path.join(RAW_DATA_DIR, 'books.jsonl')
     if not os.path.exists(fn):
-            context.error(f'Raw data path does not exist: {fn}')
+            context.log.error(f'Raw data path does not exist: {fn}')
 
     data = []
     with open(fn, 'r', encoding='utf-8') as file:
@@ -19,9 +22,9 @@ def extract(context: dg.AssetExecutionContext) -> List[str]:
             try:
                 data.append(json.loads(line))
             except json.JSONDecodeError:
-                context.error(f'Error parsing line {line_number}: {line.strip()}')
+                context.log.error(f'Error parsing line {line_number}: {line.strip()}')
     return data
 
 @dg.asset
-def transform(context: dg.AssetExecutionContext) -> None:
+def load(context: dg.AssetExecutionContext) -> None:
     pass
